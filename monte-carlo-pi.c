84055e90f9e7787dda7fc6a3ac1ad8059ecd1293
keithgr <kw28grab@siena.edu>
2017-10-17 00:10:52 -0400

    Set up random stoof

--- monte-carlo-pi.c
+++ monte-carlo-pi.c
@@ -5,10 +5,45 @@
 */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+#include <errno.h>
+
+#include "timer.h"
 
 int main(int argc, char *argv[])
 {
-  printf("%5.3f\n", drand48());
+  int rank, numprocs;
+  int numpoints;
+  
+  MPI_Init(&argc, &argv);
+  
+  if(argc != 2)
+  {
+    fprintf(stderr, "Usage: %s numpoints\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  numpoints = atoi(argv[1]);
+
+  if(errno)
+  {
+    fprintf(stderr, "Usage: %s invalid format for numpoints\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+
+  
+  /* Set up random seed */
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  srand48((unsigned int)time.tv_usec + rank);
+
+  printf("%5.9f\n", drand48());
+  
+  MPI_Finalize();
   
   return 0;
 }
