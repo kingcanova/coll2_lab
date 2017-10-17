a14a83f6dbdeb3b1cecd8c487b1501ed75ba1656
keithgr <kw28grab@siena.edu>
2017-10-17 10:54:07 -0400

    PIgit add --all!

--- monte-carlo-pi.c
+++ monte-carlo-pi.c
@@ -14,7 +14,14 @@
 int main(int argc, char *argv[])
 {
   int rank, numprocs;
+
   long numpoints;
+  long circlepoints = 0;
+  long min, max;
+  
+  double x, y;
+
+  double mypi, pi;
   
   MPI_Init(&argc, &argv);
   
@@ -40,10 +47,48 @@ int main(int argc, char *argv[])
   struct timeval time;
   gettimeofday(&time, NULL);
   srand48((unsigned int)time.tv_usec + rank);
+
+
   
   /* Compute pi */
   if(rank == 0)
-    printf("Will use %d points per process\n", numpoints);
+    printf("Will use %ld points per process\n", numpoints);
+
+  int i;
+
+  //for each point
+  for(i = 0; i < numpoints; i++)
+  {
+    //generate random coordinates
+    x = drand48();
+    y = drand48();
+
+    //if a point within the circle is found
+    //increment our count
+    //sqrt is not needed cuz math
+    if(x*x + y*y < 1.0)
+      circlepoints++;
+  }
+
+  mypi = (4.0 * circlepoints) / numpoints; 
+
+
+
+  /* Display process results */
+  printf("[%d] %ld in circle, pi approx = %1.6f\n", rank, circlepoints, mypi);
+
+  /* Get global values */
+  MPI_Reduce(&circlepoints, &min, 1, MPI_LONG, MPI_MIN, 0, MPI_COMM_WORLD);
+  MPI_Reduce(&circlepoints, &max, 1, MPI_LONG, MPI_MAX, 0, MPI_COMM_WORLD);
+
+  MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  pi = pi / numprocs;
+  
+  if(rank == 0)
+  {
+    printf("in circle values range from %ld to %ld\n", min, max);
+    printf("Final approximation of pi: %1.6f\n", pi);
+  }
   
   MPI_Finalize();
   
