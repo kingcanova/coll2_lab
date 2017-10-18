b210d304258386cc70f2979d6c70c7ecd9a88dca
kingcanova <tcanova1@gmail.com>
2017-10-17 23:36:07 -0400

    ALL ABOARD THE HYPE TRAIN

--- prefix_sum.c
+++ prefix_sum.c
@@ -17,22 +17,38 @@ int main(int argc, char *argv[])
   {
     MPI_Abort(MPI_COMM_WORLD,1);
   }
-  
+
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
 
   int myArr[numprocs];
+  int finalArr[numprocs];
 
   struct timeval time;
   gettimeofday(&time, NULL);
   srand48((unsigned int)time.tv_usec + rank);
 
+  //Init the array with random values between 0 and 9
+  for(int i = 0; i < numprocs; i++)
+  {
+    int x = drand48() * 10;
+    myArr[i] = x;
+  }
+
+  MPI_Scan(&myArr,&finalArr,numprocs,MPI_Int,MPI_SUM,MPI_COMM_WORLD);
+  printf("The prefix sums are as follows \n");
+  if(rank == 0)
+  {
+    for(int i = 0; i < numprocs; i++)
+    {
+      printf("[ %d ] ", finalArr[i]);
+    }
+  }
   
 
 
 
 
-
-
-  
+  MPI_Finalize();
+  return 0;
 }
