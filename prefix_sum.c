12c9c726e5eccd58dc6f227f0d717fb770c6d095
kingcanova <tcanova1@gmail.com>
2017-10-17 23:49:07 -0400

    Prefix sum finished

--- prefix_sum.c
+++ prefix_sum.c
@@ -21,25 +21,30 @@ int main(int argc, char *argv[])
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
 
-  int myArr[numprocs];
-  int finalArr[numprocs];
-
+  int count = atoi(argv[1]);
+  
+  int myArr[count];
+  int finalArr[count];
+  
   struct timeval time;
   gettimeofday(&time, NULL);
   srand48((unsigned int)time.tv_usec + rank);
 
   //Init the array with random values between 0 and 9
-  for(int i = 0; i < numprocs; i++)
+  for(int i = 0; i < count; i++)
   {
     int x = drand48() * 10;
     myArr[i] = x;
+    printf(" [ %d ] ",x);
   }
+  printf("\n");
 
-  MPI_Scan(&myArr,&finalArr,numprocs,MPI_Int,MPI_SUM,MPI_COMM_WORLD);
+  //Use MPI_Scan to get the prefix sum into finalArr and then have proc 0 print it out
+  MPI_Scan(&myArr,&finalArr,numprocs,MPI_INT,MPI_SUM,MPI_COMM_WORLD);
   printf("The prefix sums are as follows \n");
   if(rank == 0)
   {
-    for(int i = 0; i < numprocs; i++)
+    for(int i = 0; i < count; i++)
     {
       printf("[ %d ] ", finalArr[i]);
     }
